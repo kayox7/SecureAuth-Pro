f1bf1dd03981fa2dcacc12f98577bf6dfcf2e14d
Kayox7 <Hacker2.goku@gmail.com>
2026-07-26 13:38:05 +0530

    Add SMTP verification logging

--- config/mail.js
+++ config/mail.js
@@ -1,21 +1,21 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-
     host: process.env.MAIL_HOST,
-
     port: Number(process.env.MAIL_PORT),
-
     secure: false,
-
     auth: {
-
         user: process.env.MAIL_USER,
-
         pass: process.env.MAIL_PASS
-
     }
+});
 
+transporter.verify(function (error, success) {
+    if (error) {
+        console.error("SMTP Error:", error);
+    } else {
+        console.log("SMTP Ready");
+    }
 });
 
 module.exports = transporter;
