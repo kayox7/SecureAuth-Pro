546c10f9f863d0721741c4d2958f6fb6f06db1e0
Kayox7 <Hacker2.goku@gmail.com>
2026-07-26 15:55:03 +0530

    Fix password reset link for production

--- controllers/passwordController.js
+++ controllers/passwordController.js
@@ -204,7 +204,15 @@ exports.resetPassword = async (req, res) => {
             });
 
         }
+const passwordRegex =
+/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/;
 
+if (!passwordRegex.test(password)) {
+    return res.status(400).json({
+        success: false,
+        message: "Password must contain at least 8 characters, one uppercase letter, one lowercase letter, one number and one special character."
+    });
+}
         // Hash new password
         const hashedPassword = await bcrypt.hash(password, 12);
 
